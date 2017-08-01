4039550c1e401e1e31ae0e199633c785d281af54
方溯 <fangsu@fangsudeiMac.local>
2017-08-01 13:22:51 +0800

    Initial Commit

---
+++ BRJ_B端/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BRJ_B端
+//
+//  Created by 方溯 on 2017/8/1.
+//  Copyright © 2017年 方溯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
